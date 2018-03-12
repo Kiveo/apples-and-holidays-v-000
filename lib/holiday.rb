50c7135129bc013fef35f13e20e7736ac264325a
Maverick Tesini <mavericktesini@gmail.com>
2018-03-12 20:22:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,6 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ     etc.
   holiday_hash.each {|season|
     puts "#{season}!!"
-    season.each {|holiday, supplies|
-      puts "#{holiday}: #{supplies}"
-    }
   }
 end
 
