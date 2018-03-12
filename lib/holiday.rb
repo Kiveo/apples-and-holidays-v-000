1f2808fb58a045860bc262b4edd2632a2748cea1
Maverick Tesini <mavericktesini@gmail.com>
2018-03-12 20:34:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season, holi_hash|
     puts "#{season.capitalize}:"
     holi_hash.each do |holiday, supplies|
-      puts " #{holiday.capitalize}: #{supplies}"
+      puts " #{holiday.capitalize}: #{supplies.each {|supply| array2=[] array2 << supply array2 }}"
     end
   }
 end
